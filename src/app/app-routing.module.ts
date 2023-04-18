3f5441d4954c8260c3a8043cb85b3c6a60cf0cd8
Thien Nguyen <nguyenphongthien@yahoo.com>
2023-04-18 14:07:50 +0200

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContractOverviewComponent } from './contract/contract-overview/contract-overview.component';
+
+const routes: Routes = [{
+  path: '',
+  component: ContractOverviewComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/contract/contract-overview/contract-overview.component.ts
+++ src/app/contract/contract-overview/contract-overview.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-contract-overview',
   templateUrl: './contract-overview.component.html',
   styleUrls: ['./contract-overview.component.scss']
 })
