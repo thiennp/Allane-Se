9b988beafdcd01614987c0ccf810db8947721340
Thien Nguyen <nguyenphongthien@yahoo.com>
2023-04-22 10:45:27 +0200

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      primary: '#3f51b5'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
