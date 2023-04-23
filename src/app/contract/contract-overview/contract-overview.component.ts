470918ceb858bcc5e6830ae070f21314fd161dea
Thien Nguyen <nguyenphongthien@yahoo.com>
2023-04-23 15:03:45 +0200

    Added contract overview page

--- src/app/contract/contract-overview/contract-overview.component.ts
+++ src/app/contract/contract-overview/contract-overview.component.ts
@@ -1,9 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { BehaviorSubject, filter, ReplaySubject, Subject, takeUntil, tap } from 'rxjs';
+import { Sort } from 'src/app/common/restful-types';
+import { environment } from 'src/environments/environment';
+
+import { ContractOverviewDTO, ContractService } from '../contract.service';
+import {
+  LeasingContractDetailsDialogComponent,
+  LeasingContractDetailsDialogComponentData,
+} from '../leasing-contract-details-dialog/leasing-contract-details-dialog.component';
+import {
+  CreateLeasingContractDialogComponent,
+} from '../leasing-contract-form-dialog/create-leasing-contract-dialog.component';
 
 @Component({
   templateUrl: './contract-overview.component.html',
   styleUrls: ['./contract-overview.component.scss']
 })
-export class ContractOverviewComponent {
+export class ContractOverviewComponent implements OnDestroy {
+  public readonly displayedColumns: ReadonlyArray<string> = [
+    'contractNo',
+    'customer',
+    'vehicle',
+    'vin',
+    'monthlyRate',
+    'vehiclePrice',
+    'details',
+  ];
 
+  private readonly destroy$ = new Subject<null>();
+  public readonly numberOfItems$ = new BehaviorSubject<number>(0);
+  public readonly numberOfPages$ = new BehaviorSubject<number>(0);
+
+  public readonly pagination$ = new BehaviorSubject<PageEvent>({
+    pageIndex: 0,
+    pageSize: environment.defaultPageSize,
+    length: 0,
+  });
+
+  public readonly dataSource$ = new ReplaySubject<MatTableDataSource<ContractOverviewDTO> | null>(1);
+
+  @ViewChild('paginator') public set paginator(elementRef: ElementRef<MatPaginator> | undefined) {
+    if (elementRef) {
+      this.dataSource$.pipe(
+        filter((dataSource): dataSource is MatTableDataSource<ContractOverviewDTO> => !!dataSource),
+      ).subscribe((dataSource) => dataSource.paginator = elementRef.nativeElement);
+    }
+  };
+
+
+  constructor(
+    private readonly contractService: ContractService,
+    private readonly matDialog: MatDialog,
+  ) {
+    this.pagination$.subscribe(({ pageIndex, pageSize }) => {
+      this.contractService.getContracts({
+        page: pageIndex,
+        size: pageSize,
+        sort: Sort.ASC,
+      }).pipe(
+        takeUntil(this.destroy$),
+        tap(({ numberOfItems }) => this.numberOfItems$.next(numberOfItems)),
+        tap(({ numberOfPages }) => this.numberOfPages$.next(numberOfPages)),
+      ).subscribe((data) => this.dataSource$.next(new MatTableDataSource(data.overviewItems)));
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(null);
+  }
+
+  public createContract() {
+    this.matDialog.open<CreateLeasingContractDialogComponent, undefined, number>(
+      CreateLeasingContractDialogComponent,
+      {
+        disableClose: true,
+      })
+      .afterClosed()
+      .pipe(
+        filter((contractId) => !!contractId),
+        tap(() => this.reset()),
+      ).subscribe();
+  }
+
+  public viewContractDetails(contractId: number) {
+    this.matDialog.open<LeasingContractDetailsDialogComponent, LeasingContractDetailsDialogComponentData, number>(
+      LeasingContractDetailsDialogComponent,
+      {
+        data: {
+          contractId,
+        },
+        disableClose: true,
+      })
+      .afterClosed()
+      .pipe(
+        filter((contractId) => !!contractId),
+        tap(() => this.reset()),
+      ).subscribe();
+  }
+
+  private reset() {
+    this.numberOfItems$.next(0);
+    this.numberOfPages$.next(0);
+    this.dataSource$.next(null);
+    this.pagination$.next({
+      ...this.pagination$.value,
+      pageIndex: 0,
+    });
+  }
+
+  public updatePage(evt: PageEvent) {
+    this.pagination$.next(evt);
+  }
 }
--- src/app/contract/contract-overview/contract-overview.module.ts
+++ src/app/contract/contract-overview/contract-overview.module.ts
@@ -1,7 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 
-import { LeasingContractDialogModule } from '../leasing-contract-dialog/leasing-contract-dialog.module';
+import {
+  LeasingContractDetailsDialogModule,
+} from '../leasing-contract-details-dialog/leasing-contract-details-dialog.module';
+import { LeasingContractFormDialogModule } from '../leasing-contract-form-dialog/leasing-contract-form-dialog.module';
 import { ContractOverviewComponent } from './contract-overview.component';
 
 
@@ -12,7 +23,16 @@ import { ContractOverviewComponent } from './contract-overview.component';
   ],
   imports: [
     CommonModule,
-    LeasingContractDialogModule,
+    LeasingContractDetailsDialogModule,
+    LeasingContractFormDialogModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
   ],
 })
 export class ContractOverviewModule { }
