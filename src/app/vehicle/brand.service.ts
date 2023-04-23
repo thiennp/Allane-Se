20b5d82a1950be61886142c57b8e7b27c7dcd5ce
Thien Nguyen <nguyenphongthien@yahoo.com>
2023-04-23 10:00:18 +0200

    Created brand services

--- src/app/vehicle/brand.service.ts
+++ src/app/vehicle/brand.service.ts
@@ -1,15 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, first, iif, mergeMap, Observable, of, ReplaySubject, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 export type BrandDTO = {
   id: number;
-  brand: string;
-  model: string;
-  modelYear: number;
-  vin: string;
-  price: number;
+  name: string;
 };
 
 export type ModelDTO = {
@@ -22,18 +18,31 @@ export type ModelDTO = {
 @Injectable({
   providedIn: 'root'
 })
-export class CustomerService {
+export class BrandService {
+  public readonly brands$ = new ReplaySubject<BrandDTO[]>(1);
+  public readonly models$ = new ReplaySubject<Record<string, ModelDTO[]>>(1);
+  public readonly preloaded$ = new BehaviorSubject<Record<string, boolean>>({});
 
   constructor(
     private readonly httpClient: HttpClient
   ) {
+    this.httpClient.get<BrandDTO[]>(`${environment.apiUrl}/brands`).subscribe((brands) => this.brands$.next(brands));
+    this.models$.next({});
   }
 
-  public getBrands(): Observable<BrandDTO[]> {
-    return this.httpClient.get<BrandDTO[]>(`${environment.apiUrl}/brands`);
-  }
-
-  public getModels(brandId: string): Observable<ModelDTO[]> {
-    return this.httpClient.get<ModelDTO[]>(`${environment.apiUrl}/brand/${brandId}/models`);
+  public getModels(brandId: number): Observable<ModelDTO[]> {
+    return this.models$.pipe(
+      mergeMap((models) => iif(
+        () => !!models[brandId],
+        of(models[brandId]),
+        this.preloaded$.pipe(
+          first((preloaded) => !preloaded[brandId]),
+          tap((preloaded) => this.preloaded$.next({ ...preloaded, [brandId]: true })),
+          mergeMap(() => this.httpClient.get<ModelDTO[]>(`${environment.apiUrl}/brand/${brandId}/models`).pipe(
+            tap((brandModels) => this.models$.next({ ...models, [brandId]: brandModels })),
+          )),
+        ),
+      )),
+    );
   }
 }
--- src/app/vehicle/vehicle.service.ts
+++ src/app/vehicle/vehicle.service.ts
@@ -6,21 +6,10 @@ import { environment } from 'src/environments/environment';
 import { CommonRestfulApiService } from '../common/restful-api.service';
 import { PageRequestDTO, PageResponseDTO } from '../common/restful-types';
 
-
-export type BrandDTO = {
-  id: number;
-  brand: string;
-  model: string;
-  modelYear: number;
-  vin: string;
-  price: number;
-};
-
-export type ModelDTO = {
-  id: number;
-  name: string;
-};
-
+/*
+ * TODO: The response type of brand and model should be string here instead of number.
+ * But it is from the API so there's nothing we can do from FE side.
+ */
 export type VehicleDTO = {
   id: number;
   brand: string;
